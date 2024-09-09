d211e10f78da0a9ee22382a569bd4043be53e3f3
BoburF <changeitgo3@gmail.com>
2024-09-09 11:25:28 +0500

    [+] batman

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BoburF/bhsml/src/bhsml-asm"
+)
+
+func main() {
+
+	file, err := os.Open("index.bhsml")
+
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	fo, err := os.Create("ast.json")
+	if err != nil {
+		panic(err)
+	}
+	defer fo.Close()
+
+	err = basm.Assemble(file, fo)
+	fmt.Println("JSON data written to ast.json successfully")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/BoburF/browser
+
+go 1.22.4
+
+require github.com/BoburF/bhsml v1.0.0 // indirect
